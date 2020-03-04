d8cf13456c4e80a05b72f58b4d5c3af53dcd4aca
Hossam ELbahrawy <hossamelbahrawy52@gmail.com>
2020-03-04 19:37:00 +0200

    game logic is done

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -21,6 +21,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var cell8: UIButton!
     @IBOutlet weak var player1Name: UILabel!
     @IBOutlet weak var player2Name: UILabel!
+    @IBOutlet weak var playerNamesView: UIStackView!
+    @IBOutlet weak var winnerView: UIStackView!
+    @IBOutlet weak var winnerName: UILabel!
     
     var currentPlayer = 1
     var boardCells: [(UIButton?)] = []
@@ -29,9 +32,18 @@ class ViewController: UIViewController {
         0,0,0,
         0,0,0
     ]
+    let winPatterns = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         boardCells = [
             cell0,
             cell1,
@@ -43,29 +55,31 @@ class ViewController: UIViewController {
             cell7,
             cell8,
         ]
-        
         clearBoard()
         print("Done initializing...")
-        
+        winnerView.isHidden = true
     }
     
     @IBAction func cellPressed(_ sender: UIButton) {
-        print(sender.currentTitle!)
-        //        print(sender.currentBackgroundImage!.accessibilityIdentifier!)
         let cellNumber = Int(sender.currentTitle!)!
         if board[cellNumber] == 0 {
             play(player: currentPlayer, cell: cellNumber )
         }
-        let winner = checkWinner()
-        print(" The Winner is \(winner)")
     }
     
     @IBAction func resetGamePressed(_ sender: UIButton) {
         print("Resseting Game")
         clearBoard()
+        playerNamesView.isHidden = false
+        winnerView.isHidden = true
     }
     
     func clearBoard (){
+        board = [
+            0,0,0,
+            0,0,0,
+            0,0,0
+        ]
         player1Name.font = UIFont.init(name: "TradeWinds", size: 21)
         for cell in boardCells{
             cell!.setBackgroundImage(UIImage.init(named: "emptyCell"), for: .normal)
@@ -73,9 +87,7 @@ class ViewController: UIViewController {
     }
     
     func play (player:Int, cell:Int){
-        
         board[cell] = player
-        
         if(player == 1){
             boardCells[cell]?.setBackgroundImage(UIImage.init(named: "oCell"), for: .normal)
             player1Name.font = UIFont.init(name: "TradeWinds", size: 17)
@@ -87,8 +99,12 @@ class ViewController: UIViewController {
             player1Name.font = UIFont.init(name: "TradeWinds", size: 21)
             
         }
-        currentPlayer = changePlayer(player)
-        
+        let winner: Int? = checkWinner()
+        if (winner == nil ){
+            currentPlayer = changePlayer(player)
+        }else{
+            endGame()
+        }
     }
     
     func changePlayer(_ player: Int)-> Int{
@@ -100,8 +116,36 @@ class ViewController: UIViewController {
         }
     }
     
-    func checkWinner()-> String{
-        return "DADA"
+    func checkWinner()-> Int? {
+        for pattern in winPatterns{
+            var cnt = 0
+            for cell in pattern{
+                if(board[cell] == currentPlayer){
+                    cnt += 1
+                }
+            }
+            if(cnt == 3){
+                return currentPlayer
+            }
+            cnt = 0
+        }
+        return nil
+    }
+    
+    func endGame(){
+        board = [
+            3,3,3,
+            3,3,3,
+            3,3,3
+        ]
+        if(currentPlayer == 1 ){
+            winnerName.text = "PLAYER 1"
+        }else{
+            winnerName.text = "PLAYER 2"
+            winnerName.textColor = UIColor.init(named: "Black")
+        }
+        playerNamesView.isHidden = true
+        winnerView.isHidden = false
     }
 }
 
